ad5342a6e245db46fa94bb50fe0ec4836c8ef886
Capi Etheriel <barraponto@gmail.com>
2025-11-10 02:37:34 -0300

    feat(app): give page titles

--- pages/news.py
+++ pages/news.py
@@ -3,4 +3,8 @@ from st_utils import agent_loader, news_loader
 agent = agent_loader()
 news = news_loader()
 
+"""
+# Ragaton loves reading the news!
+"""
+
 news.ui(agent)
--- pages/youtube.py
+++ pages/youtube.py
@@ -3,4 +3,8 @@ from st_utils import agent_loader, youtube_loader
 agent = agent_loader()
 youtube = youtube_loader()
 
+"""
+# Ragaton loves watching videos!
+"""
+
 youtube.ui(agent)
